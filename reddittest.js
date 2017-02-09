7373a8ea226ffe7c34eb400570e44b9cf1ef3758
rycao18 <rycao18@gmail.com>
2017-02-09 04:00:11 +0000

    Submitting to reddit works

---
+++ reddittest.js
@@ -0,0 +1,24 @@
+var rawjs = require('raw.js');
+var reddit = new rawjs("csgobettingbot");
+reddit.setupOAuth2("RxO5CAM77N5pKg", "SWjK2mRfCIb8TEapepaW0oCLNTU", "http://google.com");
+
+reddit.auth({"username": "rycao18", "password": "#blitzpullzrichard1999"}, function(err, response) {
+    if(err) {
+        console.log("Unable to authenticate user: " + err);
+    } else {
+        // The user is now authenticated. If you want the temporary bearer token, it's available as response.access_token
+        // and will be valid for response.expires_in seconds.
+        // raw.js will automatically refresh the bearer token as it expires. Unlike web apps, no refresh tokens are available.
+        reddit.submit(
+            {
+                save: true,
+                inboxReplies: true,
+                r: 'r/csgobetting',
+                text: "TEST",
+                title: "TESTTITLE",
+            }, function(err, id) {
+                if (err) console.log(err);
+            }
+        );
+    }
+});
